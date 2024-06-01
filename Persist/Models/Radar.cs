6a209d8c5eb659c3c22072536f0c7d2447a4f9f4
Daiane Costa <cicuto.adv@gmail.com>
2024-06-01 17:28:56 -0300

    Ingestão dos dados no SQLServer

--- Persist/Models/Radar.cs
+++ Persist/Models/Radar.cs
@@ -1,15 +1,10 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Models
 {
     public class Radar
     {
         [JsonProperty("radar")]
-        public List<RadarData> Radars { get; set; }
+        public List<DadosRadares> Radars { get; set; }
     }
 }
